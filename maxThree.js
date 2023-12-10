225558dff18445a50b7b7b55a5c1a0ab7bc360c4
Saeedkpi <info.saeedbpsc.com>
2023-12-11 01:06:53 +0600

    Find Max value WithIn 3 Number

---
+++ maxThree.js
@@ -0,0 +1,13 @@
+const jim = 69;
+const dula = 97;
+const dipu = 85;
+
+if(jim > dula && jim > dipu) {
+    console.log('Jim will get the cake');
+}
+else if (dula > jim && dula > dipu) {
+    console.log('Dula will get the cake');
+}
+else {
+    console.log("Dipu will get the cake");
+}
